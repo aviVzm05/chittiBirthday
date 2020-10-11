7cc03cb0b3b251a5d4f26db0eee3cffd9edd90a4
avinash <avi.vzm05@gmail.com>
2020-10-11 20:50:03 +0530

    updated changes

--- routes/accept_bkup.js
+++ routes/accept_bkup.js
@@ -11,38 +11,62 @@ const { rejects } = require('assert');
 const { type } = require('os');
 const { json } = require('express');
 const { Readable } = require('stream');
-const os = require('os');
-require('dotenv').config();
 
 var router = express.Router();
 
 // create new storage instance
 const gc =  new Storage({
-    keyFilename: path.join(__dirname,'..',process.env.GOOGLE_APPLICATION_CREDENTIALS),
+    keyFilename: path.join(__dirname,'../nodejstestingavinash-d04991471ef5.json'),
     projectId: 'nodejstestingavinash' // storage owner....
 });
-
-const testBucket = gc.bucket(process.env.GCLOUD_STORAGE_BUCKET);
 
 // Create an instance to our firestore db ... 
 
 const db = new Firestore({
     projectId: 'nodejstestingavinash',
-    keyFilename: path.join(__dirname,'..',process.env.GOOGLE_APPLICATION_CREDENTIALS)
+    keyFilename: path.join(__dirname,'../nodejstestingavinash-2812be094e14_pojectowner.json')
   });
+
+const testBucket = gc.bucket(process.env.GCLOUD_STORAGE_BUCKET);
 
 router.use(upload({
     useTempFiles: false,
+    // tempFileDir: '../tmp/',
     debug: false
 }));
 
-//get the current OS temporary file.
-const tempPath = os.tmpdir();
+//new  promise to read control file and write the data back. ensure the function awaits for completion.
+
+function writeDataToFile(data) {
+    var writePromise = new Promise((resolve,reject) => {
+        var writeData = [];
+        var fileData = fs.readFileSync(path.join(__dirname,'..','test.txt'),'utf-8');     
+        var testdata;
+        if (fileData.length == 0){
+            writeData.push(data);
+            testdata = JSON.stringify(writeData,null,2);
+        }else {
+            var objectData = JSON.parse(fileData);
+            objectData.push(data);
+            testdata = JSON.stringify(objectData,null,2);
+        }
+        fs.writeFile(path.join(__dirname,'..','test.txt'),testdata,(err) =>{
+            if (!err){
+                resolve();
+            } else {
+                console.log(err);
+                reject('err');
+            }
+        });
+    });
+    return writePromise;
+}
 
 //create new function to copy the video to temp location. 
+
 function uploadVideoToTemp(file,filename1) {
     const uploadVideo = new Promise((resolve,reject) => {
-        file.mv(path.join(tempPath,filename1),(err)=> {
+        file.mv(path.join(__dirname,'../tmp',filename1),(err)=> {
             if (!err) {
                 resolve();
             }else {
@@ -77,7 +101,9 @@ router.post('/', function(req, res, next) {
         //code to move the vedio to temp location
         uploadVideoToTemp(req.files.upload,filename1)
         .then(() => {
-            fs.createReadStream(path.join(tempPath,filename1))
+            fs.createReadStream(path.join(__dirname,'../tmp',filename1),{
+                highWaterMark: 2 * 1024 * 1024
+            })
             .pipe(fileObject.createWriteStream({
                 gzip: true,
                 resumable:  false
@@ -91,21 +117,20 @@ router.post('/', function(req, res, next) {
                 email: req.body.email,
                 filename: filename1
                 };
-                const docRef = db.collection('Videos').doc(filename1);
-                    async function setDocument(data) {
-                        var output = await docRef.set(data);
-                    };
-                setDocument(data);
-                res.status(201);
-                res.send('ok');
-
-                console.log(`Record successfully updloaded for ${data.firstname},${data.lastname}`)
-                fs.unlink(path.join(tempPath,filename1),(err)=> {
-                    if (!err) {
-                        console.log('temp file deleted');
-                    }else{
-                        console.log('well this failed ' + err);
-                    }
+                writeDataToFile(data).then(() => {
+                    console.log(`data written successfully for ${data.firstname},${data.lastname}`);
+                    fs.unlink(path.join(__dirname,'../tmp',filename1),(err)=> {
+                        if (!err) {
+                            console.log('temp file deleted');
+                        }else{
+                            console.log('well this failed ' + err);
+                        }
+                    })
+                    res.status(201);
+                    res.send('ok');
+                }).catch((err) => {
+                    console.log('Error while writting to data file ' + err);
+                    res.send('error writting log file');
                 })
             })
         }).catch((err) => {
